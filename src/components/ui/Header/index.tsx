45512819571a45aedd3549f950473dfd1c20cc5a
whyleonardo <pleasefloyd73@gmail.com>
2023-04-10 16:29:22 -0300

    feat(Header): add Logo

--- src/components/ui/Header/index.tsx
+++ src/components/ui/Header/index.tsx
@@ -1,11 +1,13 @@
 import { LocaleSwitcher } from '@/components/buttons/LocaleSwitcher'
 import { ToggleTheme } from '@/components/buttons/ToggleTheme'
+import { Logo } from '@/components/ui/Logo'
 
 import { HStack } from '@chakra-ui/react'
 
 export const Header = () => {
 	return (
 		<HStack>
+			<Logo />
 			<ToggleTheme />
 			<LocaleSwitcher />
 		</HStack>
