0fb76fde28399213ede56d7fa5e64baa1385b588
whyleonardo <pleasefloyd73@gmail.com>
2023-04-10 16:10:33 -0300

    feat(ToggleTheme): add icons to show theme mode

--- src/components/buttons/ToggleTheme/index.tsx
+++ src/components/buttons/ToggleTheme/index.tsx
@@ -1,10 +1,36 @@
-import { Button, useColorMode } from '@chakra-ui/react'
+import { useColorMode, IconButton, chakra } from '@chakra-ui/react'
+import { Sun, Moon } from '@phosphor-icons/react'
+
+const SunIcon = chakra(Sun)
+const MoonIcon = chakra(Moon)
 
 export const ToggleTheme = () => {
 	const { colorMode, toggleColorMode } = useColorMode()
+
 	const isDark = colorMode === 'dark'
 
 	return (
-		<Button data-testid='button' onClick={toggleColorMode}>Change Theme</Button>
+		<IconButton
+			bg="none"
+			_hover={{ bg: 'none' }}
+			aria-label="Toggle Theme"
+			data-testid="button"
+			icon={
+				!isDark ? (
+					<SunIcon
+						size={25}
+						_hover={{ color: 'yellow.500' }}
+						transition="all 300ms"
+					/>
+				) : (
+					<MoonIcon
+						size={25}
+						_hover={{ color: 'yellow.500' }}
+						transition="all 300ms"
+					/>
+				)
+			}
+			onClick={toggleColorMode}
+		/>
 	)
 }
