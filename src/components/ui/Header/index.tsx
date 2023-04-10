2715591ec50b6e70a35368ca4bf1d1fa019f0689
whyleonardo <pleasefloyd73@gmail.com>
2023-04-10 16:41:24 -0300

    feat(Header): style with flex

--- src/components/ui/Header/index.tsx
+++ src/components/ui/Header/index.tsx
@@ -2,14 +2,24 @@ import { LocaleSwitcher } from '@/components/buttons/LocaleSwitcher'
 import { ToggleTheme } from '@/components/buttons/ToggleTheme'
 import { Logo } from '@/components/ui/Logo'
 
-import { HStack } from '@chakra-ui/react'
+import { HStack, Stack } from '@chakra-ui/react'
 
 export const Header = () => {
 	return (
-		<HStack>
-			<Logo />
-			<ToggleTheme />
-			<LocaleSwitcher />
+		<HStack
+			justifyContent="space-between"
+			h="4rem"
+			px="4rem"
+			shadow="0px 1px 3px rgba(0,0,0,0.6)"
+		>
+			<Stack>
+				<Logo />
+			</Stack>
+
+			<HStack spacing="8">
+				<ToggleTheme />
+				<LocaleSwitcher />
+			</HStack>
 		</HStack>
 	)
 }
