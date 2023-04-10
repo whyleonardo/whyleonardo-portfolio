470891baf36ad06944acfe5eebd0a3e71e582dce
whyleonardo <pleasefloyd73@gmail.com>
2023-04-10 16:28:57 -0300

    feat(Logo): create Logo

---
+++ src/components/ui/Logo/index.tsx
@@ -0,0 +1,20 @@
+import { chakra, useColorMode } from '@chakra-ui/react'
+import { Code } from '@phosphor-icons/react'
+
+const CodeIcon = chakra(Code)
+
+export const Logo = () => {
+	const { colorMode } = useColorMode()
+
+	const isDark = colorMode === 'dark'
+
+	return (
+		<CodeIcon
+			transition="all 300ms"
+			_hover={{ transform: 'scale(1.1)' }}
+			size={40}
+			weight="fill"
+			color={isDark ? 'gray.200' : 'gray.800'}
+		/>
+	)
+}
