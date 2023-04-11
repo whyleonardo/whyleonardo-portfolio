ad9d24d9866c81e90b38dcc07a32d620d4178aa1
whyleonardo <pleasefloyd73@gmail.com>
2023-04-11 20:10:34 -0300

    feat(Header): add sticky position to header

--- src/components/ui/Header/index.tsx
+++ src/components/ui/Header/index.tsx
@@ -6,22 +6,26 @@ import { MobileMenu } from '@/components/ui/MobileMenu'
 
 import { HStack, Stack, Spacer } from '@chakra-ui/react'
 
-export const Header = () => {
+export const Header = ({ activeSection }: { activeSection: string | null }) => {
 	return (
 		<HStack
 			as={'header'}
+			pos="sticky"
+			top="0"
 			justifyContent="space-between"
 			alignSelf="end"
 			h="4rem"
 			px="2rem"
 			shadow="md"
+			_dark={{ bgColor: 'brand.900' }}
+			bgColor={'brand.100'}
 		>
 			<Stack>
 				<Logo />
 			</Stack>
 
 			<HStack display={{ base: 'none', md: 'flex' }} spacing="4">
-				<SectionsLinks />
+				<SectionsLinks activeSection={activeSection} />
 
 				<Spacer mr="4" />
 
