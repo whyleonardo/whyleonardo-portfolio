7c23d13bf20e90dd4112de072d91f5f000d86f12
whyleonardo <pleasefloyd73@gmail.com>
2023-04-11 15:31:37 -0300

    feat(sanity-client): create sanity config

---
+++ src/services/sanity-client/index.ts
@@ -0,0 +1,16 @@
+import { createClient } from 'next-sanity'
+
+import imageUrlBuilder from '@sanity/image-url'
+
+const projectId = process.env.NEXT_PUBLIC_PROJECT_ID as string
+
+const clientConfig = {
+	projectId,
+	dataset: 'production',
+	apiVersion: '2023-03-07',
+	useCdn: false
+}
+
+export const client = createClient(clientConfig)
+
+export const sanityImageBuilder = imageUrlBuilder(clientConfig)
