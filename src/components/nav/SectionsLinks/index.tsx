fe119ab19d11984d0669a83892f82a887575ca53
whyleonardo <pleasefloyd73@gmail.com>
2023-04-11 20:19:34 -0300

    refactor(SectionsLinks): change activeSession like optional prop

--- src/components/nav/SectionsLinks/index.tsx
+++ src/components/nav/SectionsLinks/index.tsx
@@ -7,7 +7,7 @@ import { Stack } from '@chakra-ui/react'
 
 interface SectionLinksProps {
 	onClose?: () => void
-	activeSection: string | null
+	activeSection?: string | null
 }
 
 export const SectionsLinks = ({
