fe6ea66e3d90655d4dc7b66663b426ee205d015f
whyleonardo <pleasefloyd73@gmail.com>
2023-04-10 21:22:19 -0300

    feat(Layout): change text and bg global colors

--- src/layouts/Layout/index.tsx
+++ src/layouts/Layout/index.tsx
@@ -2,15 +2,21 @@ import { Inter } from 'next/font/google'
 
 import { Header } from '@/components/ui/Header'
 
-import { Container } from '@chakra-ui/react'
+import { Container, useColorMode } from '@chakra-ui/react'
 const inter = Inter({ subsets: ['latin'] })
 
 export const Layout = () => {
+	const { colorMode } = useColorMode()
+
+	const isDark = colorMode === 'dark'
+
 	return (
 		<Container
+			overflowX="hidden"
 			h="100vh"
+			color={isDark ? 'brand.100' : 'brand.900'}
 			fontFamily={inter.style.fontFamily}
-			bgColor="blackAlpha.100"
+			bgColor={!isDark ? 'brand.100' : 'brand.900'}
 		>
 			<Header />
 		</Container>
