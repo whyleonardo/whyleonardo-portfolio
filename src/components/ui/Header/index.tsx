652a75d6b8b085b875387424571f217a98de3f88
whyleonardo <pleasefloyd73@gmail.com>
2023-04-10 21:23:15 -0300

    refactor(Header): adjust some spacing and semantic to html

--- src/components/ui/Header/index.tsx
+++ src/components/ui/Header/index.tsx
@@ -1,24 +1,30 @@
 import { LocaleSwitcher } from '@/components/buttons/LocaleSwitcher'
 import { ToggleTheme } from '@/components/buttons/ToggleTheme'
+import { SectionsLinks } from '@/components/navbar/SectionsLinks'
 import { Logo } from '@/components/ui/Logo'
 
-import { HStack, Stack } from '@chakra-ui/react'
+import { HStack, Stack, Spacer } from '@chakra-ui/react'
 
 export const Header = () => {
 	return (
 		<HStack
+			as={'header'}
 			justifyContent="space-between"
 			h="4rem"
-			px="4rem"
-			shadow="0px 1px 3px rgba(0,0,0,0.6)"
+			px="2rem"
+			shadow="0px 1px 2px rgba(0,0,0,0.6)"
 		>
 			<Stack>
 				<Logo />
 			</Stack>
 
-			<HStack spacing="8">
-				<ToggleTheme />
+			<HStack spacing="4">
+				<SectionsLinks />
+
+				<Spacer mr="4" />
+
 				<LocaleSwitcher />
+				<ToggleTheme />
 			</HStack>
 		</HStack>
 	)
