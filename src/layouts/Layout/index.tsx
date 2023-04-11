cffd67283f93fbc2861ab8283cb4e8f978fa1c42
whyleonardo <pleasefloyd73@gmail.com>
2023-04-11 20:12:13 -0300

    feat: create basic mockup in layout

--- src/layouts/Layout/index.tsx
+++ src/layouts/Layout/index.tsx
@@ -1,24 +1,100 @@
 import { Inter } from 'next/font/google'
+import { useEffect, useState } from 'react'
 
 import { Header } from '@/components/ui/Header'
 
-import { Container, useColorMode } from '@chakra-ui/react'
+import { Container, Flex, Box, useColorMode } from '@chakra-ui/react'
 const inter = Inter({ subsets: ['latin'] })
 
 export const Layout = () => {
 	const { colorMode } = useColorMode()
+	const [activeSection, setActiveSection] = useState(null)
 
 	const isDark = colorMode === 'dark'
 
+	useEffect(() => {
+		function handleScroll() {
+			const sections = document.querySelectorAll('[data-section]')
+			const viewHeight = window.innerHeight
+			const viewTop = window.pageYOffset
+			const viewBottom = viewTop + viewHeight
+
+			// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+			// @ts-ignore
+			for (const section of sections) {
+				const rect = section.getBoundingClientRect()
+				const sectionTop = rect.top + viewTop
+				const sectionBottom = sectionTop + rect.height
+
+				if (sectionTop <= viewBottom && sectionBottom >= viewTop) {
+					setActiveSection(section.getAttribute('data-section'))
+					break
+				}
+			}
+		}
+
+		window.addEventListener('scroll', handleScroll)
+		return () => window.removeEventListener('scroll', handleScroll)
+	}, [])
+
 	return (
 		<Container
-			overflowX="hidden"
-			h="100vh"
 			color={isDark ? 'brand.100' : 'brand.900'}
 			fontFamily={inter.style.fontFamily}
 			bgColor={!isDark ? 'brand.100' : 'brand.900'}
+			scrollBehavior="smooth"
 		>
-			<Header />
+			<Box pos="absolute" top="0" data-section="home" id="home" />
+
+			<Header activeSection={activeSection} />
+
+			<Flex h="calc(600px - 5rem)" border="1px">
+				Home
+			</Flex>
+
+			<Flex
+				h="1600px"
+				mt="20px"
+				pt="5rem"
+				border="1px"
+				data-section="about"
+				id="about"
+			>
+				About
+			</Flex>
+
+			<Flex
+				h="1600px"
+				mt="20px"
+				pt="5rem"
+				border="1px"
+				data-section="skills"
+				id="skills"
+			>
+				Skills
+			</Flex>
+
+			<Flex
+				h="1600px"
+				mt="20px"
+				pt="5rem"
+				border="1px"
+				data-section="projects"
+				id="projects"
+			>
+				Projects
+			</Flex>
+
+			<Flex
+				h="1600px"
+				mt="20px"
+				pt="5rem"
+				border="1px"
+				data-section="contact"
+				id="contact"
+			>
+				Contact
+			</Flex>
 		</Container>
 	)
 }
