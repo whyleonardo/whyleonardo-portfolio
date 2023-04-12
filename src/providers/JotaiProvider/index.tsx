65bbc47b1942b8b75c08b01764af8597b7199331
whyleonardo <pleasefloyd73@gmail.com>
2023-04-12 18:45:33 -0300

    feat: create jotai provider

---
+++ src/providers/JotaiProvider/index.tsx
@@ -0,0 +1,7 @@
+import { ReactNode } from 'react'
+
+import { Provider } from 'jotai'
+
+export const JotaiProvider = ({ children }: { children: ReactNode }) => {
+	return <Provider>{children}</Provider>
+}
