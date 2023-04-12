d5cadf080d2b115257ffe820b69370084f814fa7
whyleonardo <pleasefloyd73@gmail.com>
2023-04-12 18:45:09 -0300

    refactor: remove undefined type from function return

--- src/utils/getPersonalInfo/index.ts
+++ src/utils/getPersonalInfo/index.ts
@@ -4,7 +4,7 @@ import { PersonalInfo } from '@/types/personal-info'
 export async function getPersonalInfo() {
 	const data: PersonalInfo[] = await client.fetch('*[_type == "personalInfo"]')
 
-	const personalInfo = data.at(0)
+	const personalInfo = data.at(0) as PersonalInfo
 
 	return personalInfo
 }
