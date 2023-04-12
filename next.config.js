408d5a2dd816a6f4d0b39cb33f8d87a4eab455aa
whyleonardo <pleasefloyd73@gmail.com>
2023-04-12 17:24:45 -0300

    chore(next.config): add remote pattern images config

--- next.config.js
+++ next.config.js
@@ -4,7 +4,16 @@
 const nextTranslate = require('next-translate-plugin')
 
 const nextConfig = {
-	reactStrictMode: true
+	reactStrictMode: true,
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'cdn.sanity.io',
+				pathname: '/images/**'
+			}
+		]
+	}
 }
 
 module.exports = nextTranslate(nextConfig)
