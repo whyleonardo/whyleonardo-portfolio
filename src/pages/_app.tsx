4aea2d7feada66cb69ad70823ba871a4f1238e51
whyleonardo <pleasefloyd73@gmail.com>
2023-04-12 18:47:39 -0300

    feat: add jotai provider wrapping app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,14 @@ import '@/styles/global.css'
 import type { AppProps } from 'next/app'
 
 import { ChakraProvider } from '@/providers/ChakraProvider'
+import { JotaiProvider } from '@/providers/JotaiProvider'
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
-		<ChakraProvider>
-			<Component {...pageProps} />
-		</ChakraProvider>
+		<JotaiProvider>
+			<ChakraProvider>
+				<Component {...pageProps} />
+			</ChakraProvider>
+		</JotaiProvider>
 	)
 }
