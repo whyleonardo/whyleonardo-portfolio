47d7a43ab964338c8da6fc117d1eb5b57f8aa3f5
whyleonardo <pleasefloyd73@gmail.com>
2023-04-12 17:26:15 -0300

    feat(ProfilePhoto): create profile photo component

---
+++ src/components/ui/ProfilePhoto/index.tsx
@@ -0,0 +1,21 @@
+import { sanityImageBuilder } from '@/services/sanity-client'
+import { animation } from '@/styles/chakra-ui/animations/ProfilePhoto'
+import { PersonalInfo } from '@/types/personal-info'
+import { Image } from '@chakra-ui/next-js'
+
+type ProfilePhotoProps = Pick<PersonalInfo, 'profilePhoto'>
+
+export const ProfilePhoto = ({ profilePhoto }: ProfilePhotoProps) => {
+	const image = sanityImageBuilder.image(profilePhoto).url()
+
+	return (
+		<Image
+			animation={animation}
+			rounded="60% 40% 30% 70%/60% 30% 70% 40%"
+			width={200}
+			height={200}
+			alt="Profile Photo"
+			src={image}
+		/>
+	)
+}
