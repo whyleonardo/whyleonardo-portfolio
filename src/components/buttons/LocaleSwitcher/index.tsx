267c1d17a126386e28ec65aa257cf95a91ec3d15
Christian Leonardo <pleasefloyd73@gmail.com>
2023-04-13 17:24:12 +0000

    fix(LocaleSwitcher): remove scroll to top when locale is switched

--- src/components/buttons/LocaleSwitcher/index.tsx
+++ src/components/buttons/LocaleSwitcher/index.tsx
@@ -2,7 +2,7 @@ import { useRouter } from 'next/router'
 import { ReactCountryFlag } from 'react-country-flag'
 
 import { Link } from '@chakra-ui/next-js'
-import { Stack, chakra } from '@chakra-ui/react'
+import { IconButton, Stack, chakra } from '@chakra-ui/react'
 
 const ChakraFlag = chakra(ReactCountryFlag)
 
@@ -10,26 +10,35 @@ export const LocaleSwitcher = () => {
 	const router = useRouter()
 	const { locales, locale: activeLocale } = router
 
+	function handleChangeLocale(locale: string) {
+		router.push({}, undefined, {
+			scroll: false,
+			locale: locale
+		})
+	}
 	return (
 		<Stack direction="row" spacing="2">
 			{locales &&
 				locales.map((locale) => (
-					<Link
+					<IconButton
 						key={locale}
-						href={`/${locale == 'pt-BR' ? '' : locale}`}
-						locale={locale}
-					>
-						<ChakraFlag
-							fontSize={40}
-							countryCode={locale.slice(3)}
-							transition="all 250ms"
-							_hover={{ filter: 'grayscale(0%)' }}
-							filter={
-								activeLocale !== locale ? 'grayscale(100%)' : 'grayscale(0%)'
-							}
-							svg
-						/>
-					</Link>
+						bg="none"
+						_hover={{ bg: 'none' }}
+						aria-label={`${locale} Flag`}
+						icon={
+							<ChakraFlag
+								fontSize={40}
+								countryCode={locale.slice(3)}
+								transition="all 250ms"
+								_hover={{ filter: 'grayscale(0%)' }}
+								filter={
+									activeLocale !== locale ? 'grayscale(100%)' : 'grayscale(0%)'
+								}
+								svg
+							/>
+						}
+						onClick={() => handleChangeLocale(locale)}
+					></IconButton>
 				))}
 		</Stack>
 	)
