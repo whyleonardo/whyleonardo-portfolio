95e5a9553271cff1f13f72ce1632509710524366
Christian Leonardo <pleasefloyd73@gmail.com>
2023-04-14 00:44:36 +0000

    refactor(ProjectCard): change width

--- src/components/cards/ProjectCard/index.tsx
+++ src/components/cards/ProjectCard/index.tsx
@@ -5,7 +5,7 @@ import { Card } from '@chakra-ui/react'
 
 export const ProjectCard = ({ project }: { project: Project }) => {
 	return (
-		<Card w="400px" h="300px" transition="all 300ms">
+		<Card w="full" h="300px" transition="all 300ms">
 			{project.stack.map((tech) => (
 				<TechBadge key={tech._id} tech={tech} />
 			))}
