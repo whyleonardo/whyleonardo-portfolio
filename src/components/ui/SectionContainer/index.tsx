6c32b731935e25ce228dd6789c580a92f7d048a8
whyleonardo <pleasefloyd73@gmail.com>
2023-04-19 15:55:59 -0300

    refactor(SectionContainer): change container to grid component

--- src/components/ui/SectionContainer/index.tsx
+++ src/components/ui/SectionContainer/index.tsx
@@ -1,33 +1,61 @@
 import useTranslation from 'next-translate/useTranslation'
 import { ReactNode } from 'react'
 
-import { Heading, Flex, Stack, StackProps } from '@chakra-ui/react'
+import { Heading, Grid } from '@chakra-ui/react'
 
-interface SectionContainerProps extends StackProps {
+interface SectionContainerProps {
 	children: ReactNode
 	section: string
 }
 
 export const SectionContainer = ({
 	children,
-	section,
-	...rest
+	section
 }: SectionContainerProps) => {
 	const { t } = useTranslation('common')
 
 	return (
-		<Stack
-			as={'section'}
-			align="center"
-			direction="column"
-			minH={section === 'home' ? 'calc(100vh - 7rem)' : '100vh'}
-			pt="5rem"
-			pb="1rem"
+		// <Stack
+		// 	as={'section'}
+		// 	align="center"
+		// 	direction="column"
+		// 	minH={section === 'home' ? 'calc(100vh - 5rem)' : '100vh'}
+		// 	pt="5rem"
+		// 	pb="1rem"
+		// 	data-section={section}
+		// 	id={section}
+		// 	{...rest}
+		// >
+		// 	<Heading
+		// 		display={section === 'home' ? 'none' : 'block'}
+		// 		fontSize="3rem"
+		// 		fontWeight="thin"
+		// 		textAlign="center"
+		// 		_hover={{
+		// 			bgGradient: 'linear(to-r, brand.500, brand.700)',
+		// 			bgClip: 'text',
+		// 			color: 'transparent'
+		// 		}}
+		// 		transition="all 300ms"
+		// 	>
+		// 		{t(`sections.${section}`)}
+		// 	</Heading>
+
+		// 	{children}
+		// </Stack>
+		<Grid
+			gap="2"
+			templateAreas={`
+		"h2"
+		"section"
+		`}
+			minH={section === 'home' ? 'calc(100vh - 5rem)' : '100vh'}
+			data-section={section}
 			id={section}
-			{...rest}
 		>
 			<Heading
 				display={section === 'home' ? 'none' : 'block'}
+				pt="5rem"
 				fontSize="3rem"
 				fontWeight="thin"
 				textAlign="center"
@@ -41,7 +69,7 @@ export const SectionContainer = ({
 				{t(`sections.${section}`)}
 			</Heading>
 
-			<Flex>{children}</Flex>
-		</Stack>
+			{children}
+		</Grid>
 	)
 }
