3bb6785be6bebd56f1895e2cd83ee760f34db484
whyleonardo <pleasefloyd73@gmail.com>
2023-04-19 15:53:45 -0300

    feat: create details card list

---
+++ src/constants/details/index.ts
@@ -0,0 +1,19 @@
+import { Fire, DeviceMobile, Lightning } from '@phosphor-icons/react'
+
+export const details = [
+	{
+		title: 'are-responsive',
+		icon: DeviceMobile,
+		id: 'udfasdqwkdfldkmf'
+	},
+	{
+		title: 'look-professional',
+		icon: Fire,
+		id: 'udfkdfldkm31f'
+	},
+	{
+		title: 'load-fast',
+		icon: Lightning,
+		id: 'udfkdjrkswrfldkmf'
+	}
+]
