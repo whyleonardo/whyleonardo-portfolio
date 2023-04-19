2d97c74994ad5478928cc8366d3d9e8674b717e1
whyleonardo <pleasefloyd73@gmail.com>
2023-04-19 15:55:25 -0300

    feat(ProfilePhoto): add grayscale to photo and hover to remove

--- src/components/ui/ProfilePhoto/index.tsx
+++ src/components/ui/ProfilePhoto/index.tsx
@@ -12,8 +12,14 @@ export const ProfilePhoto = ({ profilePhoto }: ProfilePhotoProps) => {
 		<Image
 			w={{ base: '15rem', md: '17rem', lg: '22rem' }}
 			h={{ base: '15rem', md: '17rem', lg: '22rem' }}
+			border="6px solid"
+			borderColor="rgba(80, 70, 80, 0.2)"
+			_hover={{ filter: 'grayscale(0%)' }}
+			_dark={{ borderColor: 'rgba(255, 255, 255, 0.2)' }}
 			animation={animation}
+			transition="all 300ms"
 			alt="Profile Photo"
+			filter="grayscale(100%)"
 			rounded="60% 40% 30% 70%/60% 30% 70% 40%"
 			src={image}
 		/>
