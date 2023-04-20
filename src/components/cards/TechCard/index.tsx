b9c1ae2abb5b2def6a8b3ece375f164736d3bac8
whyleonardo <pleasefloyd73@gmail.com>
2023-04-20 15:47:23 -0300

    feat(TechCard): create component

---
+++ src/components/cards/TechCard/index.tsx
@@ -0,0 +1,25 @@
+import { Flex, Heading, chakra } from '@chakra-ui/react'
+
+export const TechCard = ({ tech }: { tech: any }) => {
+	const Icon = chakra(tech.icon)
+
+	return (
+		<Flex
+			key={tech.name}
+			align="center"
+			justify={{ base: 'center', lg: 'flex-start' }}
+			gap="4"
+			w="full"
+			p="4"
+			shadow="md"
+			_dark={{ bgColor: 'brand.800' }}
+			bgColor="white"
+			rounded="5px"
+		>
+			<Icon size={40} />
+			<Heading fontWeight="normal" size="xl">
+				{tech.name}
+			</Heading>
+		</Flex>
+	)
+}
