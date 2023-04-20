8a116e3d7cd6c12b3a366122384cb193a00620e1
whyleonardo <pleasefloyd73@gmail.com>
2023-04-20 15:42:21 -0300

    feat: create techs list

---
+++ src/constants/techs/index.ts
@@ -0,0 +1,60 @@
+import {
+	SiHtml5,
+	SiCss3,
+	SiJavascript,
+	SiTypescript,
+	SiReact,
+	SiNextdotjs,
+	SiGit,
+	SiChakraui,
+	SiTailwindcss,
+	SiNodedotjs,
+	SiFirebase
+} from 'react-icons/si'
+
+export const techs = [
+	{
+		name: 'HTML',
+		icon: SiHtml5
+	},
+	{
+		name: 'CSS',
+		icon: SiCss3
+	},
+	{
+		name: 'Javascript',
+		icon: SiJavascript
+	},
+	{
+		name: 'Typescript',
+		icon: SiTypescript
+	},
+	{
+		name: 'React',
+		icon: SiReact
+	},
+	{
+		name: 'Next.js',
+		icon: SiNextdotjs
+	},
+	{
+		name: 'Node',
+		icon: SiNodedotjs
+	},
+	{
+		name: 'GIT',
+		icon: SiGit
+	},
+	{
+		name: 'Chakra UI',
+		icon: SiChakraui
+	},
+	{
+		name: 'TailwindCSS',
+		icon: SiTailwindcss
+	},
+	{
+		name: 'Firebase',
+		icon: SiFirebase
+	}
+]
