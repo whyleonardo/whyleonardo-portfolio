235f0dab5d1d35676742c40d707f08bace52aaac
whyleonardo <pleasefloyd73@gmail.com>
2023-04-20 15:45:51 -0300

    feat(TechBadge): change styles

--- src/components/ui/TechBadge/index.tsx
+++ src/components/ui/TechBadge/index.tsx
@@ -1,18 +1,22 @@
+import { Rubik } from 'next/font/google'
+
 import { Tech } from '@/types/tech'
 import { Tag, TagLabel } from '@chakra-ui/react'
+
+const rubik = Rubik({ subsets: ['latin'] })
 
 export const TechBadge = ({ tech }: { tech: Tech }) => {
 	return (
 		<Tag
 			w="max-content"
-			mx="1"
-			fontFamily="sans-serif"
-			fontWeight="semibold"
+			h="1rem"
+			m="1"
+			fontFamily={rubik.style.fontFamily}
+			letterSpacing="wide"
 			textTransform="capitalize"
-			_hover={{ transform: 'scale(1.1)' }}
-			cursor="pointer"
-			transition="all 300ms"
+			_dark={{ bgColor: '#4E5B60', color: 'gray.100', opacity: '0.9' }}
 			colorScheme={tech.color}
+			rounded="20px"
 		>
 			<TagLabel>{tech.name}</TagLabel>
 		</Tag>
