6a5348bce8d4d7d0d1a5affa4cd8296a8065decd
whyleonardo <pleasefloyd73@gmail.com>
2023-04-20 15:43:48 -0300

    refactor(ProjectsSection): remove padding and add margin in container

--- src/layouts/Sections/Projects/index.tsx
+++ src/layouts/Sections/Projects/index.tsx
@@ -10,7 +10,6 @@ export const ProjectsSection = () => {
 	const [projects] = useAtom(projectsStore)
 	return (
 		<Grid
-			justifyContent="center"
 			gap="4"
 			templateColumns={{
 				base: 'repeat(1, 1fr)',
@@ -18,7 +17,7 @@ export const ProjectsSection = () => {
 				lg: 'repeat(3, 1fr)'
 			}}
 			w="full"
-			p="2rem"
+			mt="2rem"
 		>
 			{projects.map((project) => (
 				<ProjectCard key={project._id} project={project} />
