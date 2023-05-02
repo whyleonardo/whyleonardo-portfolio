c9b916daa4305745e4bddf66bd0685bd4bea749c
whyleonardo <pleasefloyd73@gmail.com>
2023-05-02 16:48:29 -0300

    feat: add vercel web analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,14 @@ import type { AppProps } from 'next/app'
 
 import { ChakraProvider } from '@/providers/ChakraProvider'
 import { JotaiProvider } from '@/providers/JotaiProvider'
+import { Analytics } from '@vercel/analytics/react'
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<JotaiProvider>
 			<ChakraProvider>
 				<Component {...pageProps} />
+				<Analytics />
 			</ChakraProvider>
 		</JotaiProvider>
 	)
